9a31e98ed09b702a1baa81cf959c927e4b3232ae
Benedette <rodrigo.benedette@mercadolivre.com>
2021-06-11 18:18:47 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,13 @@ const showData = (result)=>{
     }
 }
 
-
 e2e.addEventListener("blur",(e)=>{
     let search = e2e.value
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
 
-    fetch(`https://api.mercadopago.com/pos/${search}?access_token=APP_USR-3703711902928676-012817-ad98593253d083aca15cdd0680a9a52f-519628608`,{headers: {'Access-Control-Allow-Origin':'https://api.mercadopago.com/'}}, options)
+    fetch(`https://api.mercadopago.com/pos/${search}?access_token=APP_USR-3703711902928676-012817-ad98593253d083aca15cdd0680a9a52f-519628608`)
+
     .then(response =>{ response.json()
-        .then( data => showData(data))
+        .then( data => window.alert(JSON.stringify(data)))
     })
-    .catch(e => console.log('Deu Erro: '+ e,message))
+    .catch(e => console.log('Deu Erro: '+ e.message))
 })
